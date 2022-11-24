22efa207f144be3a3dbdd17690af43af0a3ef1cf
Bob Strahan <strahanr@amazon.com>
2022-11-24 19:17:02 +0000

    Add header

--- patches/qnabot/lambda_schema_qna.js
+++ patches/qnabot/lambda_schema_qna.js
@@ -1,3 +1,7 @@
+/*
+Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+SPDX-License-Identifier: Apache-2.0
+ */
 module.exports={
     type:"object",
     description:"Question and Answer document",
