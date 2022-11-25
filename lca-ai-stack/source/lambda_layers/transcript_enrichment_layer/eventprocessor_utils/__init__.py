fd074080c902daf0ee39ca6c9a834fdd518c0434
Babu Srinivasan <babusri@amazon.com>
2022-11-25 08:48:38 -0600

    Added license headers

--- lca-ai-stack/source/lambda_layers/transcript_enrichment_layer/eventprocessor_utils/__init__.py
+++ lca-ai-stack/source/lambda_layers/transcript_enrichment_layer/eventprocessor_utils/__init__.py
@@ -1,3 +1,6 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
 from .eventprocessor import (
     normalize_transcript_segment, 
     get_ttl,
--- lca-ai-stack/source/lambda_layers/transcript_enrichment_layer/eventprocessor_utils/eventprocessor.py
+++ lca-ai-stack/source/lambda_layers/transcript_enrichment_layer/eventprocessor_utils/eventprocessor.py
@@ -1,3 +1,6 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
 from typing import TYPE_CHECKING, Any, Coroutine, Dict, List, Literal, Optional
 from datetime import datetime, timedelta
 from os import getenv
