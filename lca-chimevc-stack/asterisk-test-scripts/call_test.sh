022add32e24dd23aec4a1f8d94fb557bc382e999
Bob Strahan <strahanr@amazon.com>
2023-02-18 23:38:12 +0000

    Improvements to test scripts and README.. One file to edit, no need to copy recordings, works without local Asterisk

--- lca-chimevc-stack/asterisk-test-scripts/call_test.sh
+++ lca-chimevc-stack/asterisk-test-scripts/call_test.sh
@@ -2,38 +2,28 @@
 
 set -eu
 
-CALLER_VC_ENDPOINT='abcdef.voiceconnector.chime.aws'
-CALLER_PHONE_NUMBER='+1571AAABBBB'
-AGENT_PHONE_NUMBER='+1618CCCDDDD' 
+# set configuration variables
+. ./common.sh
 
 RECORDING_ID=${1:-50}
 
-RECORDING_FILE_CALLER="/root/recordings/Auto_Repairs_${RECORDING_ID}_UTC_caller.wav"
+RECORDINGS_DIR=`pwd`/recordings
+RECORDING_FILE_CALLER="${RECORDINGS_DIR}/Auto_Repairs_${RECORDING_ID}_UTC_caller.wav"
 [[ -f "$RECORDING_FILE_CALLER" ]] || {
     echo "[ERROR] could not find caller recording file: ${RECORDING_FILE_CALLER}" >&2
     exit 1
 }
-RECORDING_FILE_AGENT="/var/lib/asterisk/sounds/en/Auto_Repairs_${RECORDING_ID}_UTC_agent.wav"
+RECORDING_FILE_AGENT="${RECORDINGS_DIR}/Auto_Repairs_${RECORDING_ID}_UTC_agent.wav"
 [[ -f "$RECORDING_FILE_AGENT" ]] || {
     echo "[ERROR] could not find agent recording file: ${RECORDING_FILE_AGENT}" >&2
     exit 1
 }
 
+# configure audio file in Asterisk
+setup_asterisk $RECORDING_FILE_AGENT
+
 LISTEN_PORT=$((6000 + ${RECORDING_ID} + $RANDOM % 1000))
-
-RECORDING_AGENT_BASENAME=$(basename "$RECORDING_FILE_AGENT")
-RECORDING_AGENT_EXTENSION="${RECORDING_AGENT_BASENAME##*.}"
-RECORDING_AGENT_FILENAME="${RECORDING_AGENT_BASENAME%.*}"
-ASTERISK_RECORDING_FILE_AGENT=$(basename "$RECORDING_AGENT_FILENAME" ".${RECORDING_AGENT_EXTENSION}")
-ASTERISK_EXTENSIONS_FILE='/etc/asterisk/extensions.conf'
-
-sed -i -E -e '/catch-all/,/from-phone/ s/^( *[^;].*Playback\()[^)]+(.*)/\1'"$ASTERISK_RECORDING_FILE_AGENT"'\2/' \
-  "$ASTERISK_EXTENSIONS_FILE"
-
-# systemctl restart asterisk
-rasterisk -x "core reload"
-
-/root/pjproject/pjsip-apps/bin/pjsua-aarch64-unknown-linux-gnu \
+/root/pjproject/pjsip-apps/bin/pjsua-*-unknown-linux-gnu \
     --id "sip:${CALLER_PHONE_NUMBER}@127.0.0.1" \
     --local-port "$LISTEN_PORT" \
     --play-file "$RECORDING_FILE_CALLER" \
