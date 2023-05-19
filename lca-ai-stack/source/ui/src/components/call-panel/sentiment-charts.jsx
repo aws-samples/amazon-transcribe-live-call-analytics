1edea90308ccc7d3403d69c656b81ec98e930b80
Babu Srinivasan <babusri@amazon.com>
2023-05-19 10:50:37 -0500

    revert ui changes

--- lca-ai-stack/source/ui/src/components/call-panel/sentiment-charts.jsx
+++ lca-ai-stack/source/ui/src/components/call-panel/sentiment-charts.jsx
@@ -36,7 +36,7 @@ export const VoiceToneFluctuationChart = ({ item, callTranscriptPerCallId }) => 
 
   return (
     <LineChart
-      height={300}
+      height="180"
       hideFilter
       series={[
         {
@@ -52,21 +52,11 @@ export const VoiceToneFluctuationChart = ({ item, callTranscriptPerCallId }) => 
           valueFormatter: (e) => e.toFixed(3),
         },
       ]}
-      yDomain={[-1, 1]}
+      yDomain={[-5, 5]}
       i18nStrings={{
         legendAriaLabel: 'Legend',
         chartAriaRoleDescription: 'line chart',
-        // xTickFormatter: (e) => e.toISOString().substr(14, 5),
-        xTickFormatter: (e) =>
-        e.toLocaleDateString('en-US', { month: 'short',
-                                        day: 'numeric',
-                                        hour: 'numeric',
-                                        minute: 'numeric',
-                                        second: 'numeric',
-                                        hour12: false,
-                                      }
-                            ).split(',')
-                             .join('\n'),
+        xTickFormatter: (e) => e.toISOString().substr(14, 5),
         yTickFormatter: (e) => getWeightedSentimentLabel(e),
       }}
       empty={
@@ -114,7 +104,7 @@ export const SentimentFluctuationChart = ({ item, callTranscriptPerCallId }) => 
 
   return (
     <LineChart
-      height={300}
+      height="80"
       hideFilter
       series={[
         {
@@ -130,21 +120,11 @@ export const SentimentFluctuationChart = ({ item, callTranscriptPerCallId }) => 
           valueFormatter: (e) => e.toFixed(3),
         },
       ]}
-      yDomain={[-1, 1]}
+      yDomain={[-5, 5]}
       i18nStrings={{
         legendAriaLabel: 'Legend',
         chartAriaRoleDescription: 'line chart',
-        // xTickFormatter: (e) => e.toISOString().substr(14, 5),
-        xTickFormatter: (e) =>
-          e.toLocaleDateString('en-US', { month: 'short',
-                                          day: 'numeric',
-                                          hour: 'numeric',
-                                          minute: 'numeric',
-                                          second: 'numeric',
-                                          hour12: false,
-                                        }
-                              ).split(',')
-                               .join('\n'),
+        xTickFormatter: (e) => e.toISOString().substr(14, 5),
         yTickFormatter: (e) => getWeightedSentimentLabel(e),
       }}
       empty={
