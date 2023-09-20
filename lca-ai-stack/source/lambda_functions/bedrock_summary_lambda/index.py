7a3ed95403d0652f4a648e83a5deacfa378ea484
Christopher Lott <cmlott@amazon.com>
2023-09-19 21:34:34 -0700

    add pca-style prompting for summarizaiton, where you can provide a single prompt, or a json with set of key value pairs of prompts.

--- lca-ai-stack/source/lambda_functions/bedrock_summary_lambda/index.py
+++ lca-ai-stack/source/lambda_functions/bedrock_summary_lambda/index.py
@@ -17,14 +17,34 @@ BEDROCK_MODEL_ID = os.environ["BEDROCK_MODEL_ID"]
 FETCH_TRANSCRIPT_LAMBDA_ARN = os.environ['FETCH_TRANSCRIPT_LAMBDA_ARN']
 PROCESS_TRANSCRIPT = (os.getenv('PROCESS_TRANSCRIPT', 'False') == 'True')
 TOKEN_COUNT = int(os.getenv('TOKEN_COUNT', '0')) # default 0 - do not truncate.
-SUMMARY_PROMPT_TEMPLATE = os.environ["SUMMARY_PROMPT_TEMPLATE"]
+SUMMARY_PROMPT_TEMPLATE = os.getenv('SUMMARY_PROMPT_TEMPLATE','')
+SUMMARY_PROMPT_SSM_PARAMETER = os.environ["SUMMARY_PROMPT_SSM_PARAMETER"]
 
 # Optional environment variables allow region / endpoint override for bedrock Boto3
 BEDROCK_REGION = os.environ["BEDROCK_REGION_OVERRIDE"] if "BEDROCK_REGION_OVERRIDE" in os.environ else os.environ["AWS_REGION"]
 BEDROCK_ENDPOINT_URL = os.environ.get("BEDROCK_ENDPOINT_URL", f'https://bedrock.{BEDROCK_REGION}.amazonaws.com')
 
 lambda_client = boto3.client('lambda')
+ssmClient = boto3.client("ssm")
 bedrock = boto3.client(service_name='bedrock', region_name=BEDROCK_REGION, endpoint_url=BEDROCK_ENDPOINT_URL) 
+
+def get_templates_from_ssm():
+    global SUMMARY_PROMPT_TEMPLATE
+    templates = []
+    try:
+        SUMMARY_PROMPT_TEMPLATE = ssmClient.get_parameter(Name=SUMMARY_PROMPT_SSM_PARAMETER)["Parameter"]["Value"]
+
+        prompt_templates = json.loads(SUMMARY_PROMPT_TEMPLATE)
+        for k, v in prompt_templates.items():
+            prompt = v.replace("<br>", "\n")
+            templates.append({ k:prompt })
+    except:
+        prompt = SUMMARY_PROMPT_TEMPLATE.replace("<br>", "\n")
+        templates.append({
+            "Summary": prompt
+        })
+        print("Prompt: ",prompt)
+    return templates
 
 def get_transcripts(callId):
     payload = {
@@ -41,8 +61,7 @@ def get_transcripts(callId):
     print("Lambda response:", response)
     return response
 
-def get_summary(prompt_data):
-
+def call_bedrock(prompt_data):
     modelId = BEDROCK_MODEL_ID
     accept = 'application/json'
     contentType = 'application/json'
@@ -51,24 +70,42 @@ def get_summary(prompt_data):
     provider = modelId.split(".")[0]
 
     if provider == "amazon":
-        body = json.dumps({"inputText": prompt_data})        
+        body = json.dumps({"inputText": prompt_data, "temperature":0 }) 
         response = bedrock.invoke_model(body=body, modelId=modelId, accept=accept, contentType=contentType)
         response_body = json.loads(response.get('body').read())
         summary_text = response_body.get('results')[0].get('outputText')
 
     elif provider == "ai21":
-        body = json.dumps({"prompt": prompt_data})
+        body = json.dumps({"prompt": prompt_data, "temperature":0 })
         response = bedrock.invoke_model(body=body, modelId=modelId, accept=accept, contentType=contentType)
         response_body = json.loads(response.get('body').read())
         summary_text = response_body.get('completions')[0].get('data').get('text')
 
     elif provider == "anthropic":
-        body = json.dumps({"prompt": prompt_data, "max_tokens_to_sample": 512})
+        body = json.dumps({"prompt": prompt_data, "max_tokens_to_sample": 512, "temperature":0 })
         response = bedrock.invoke_model(body=body, modelId=modelId, accept=accept, contentType=contentType)
         response_body = json.loads(response.get('body').read())
         summary_text = response_body.get('completion')
 
     return summary_text
+
+
+def generate_summary(transcript):
+    # first check to see if this is one prompt, or many prompts as a json
+    templates = get_templates_from_ssm()
+    result = {}
+    for item in templates:
+        key = list(item.keys())[0]
+        prompt = item[key]
+        prompt = prompt.replace("{transcript}", transcript)
+        response = call_bedrock(prompt)
+        print("API Response:", response)
+        result[key] = response
+    if len(result.keys()) == 1:
+        # there's only one summary in here, so let's return just that.
+        # this may contain json or a string.
+        return result[list(result.keys())[0]]
+    return json.dumps(result)
 
 def handler(event, context):
     print("Received event: ", json.dumps(event))
@@ -78,18 +115,16 @@ def handler(event, context):
     print("Transcript data:", transcript_data)
     transcript_json = json.loads(transcript_data)
     transcript = transcript_json['transcript']
-    prompt = SUMMARY_PROMPT_TEMPLATE.replace("<br>", "\n").replace("{transcript}", transcript)
-    print("Prompt: ",prompt)
-
+    summary_json = None
+    summary = "No summary available"
     try:
-        summaryText  = get_summary(prompt_data=prompt)
-
-    except Exception as err:
-        summaryText = "Error calling Bedrock API. Unable to get call summary. See logs for error details"
-        logger.error(err)
-
-    print("Summary: ", summaryText)
-    return {"summary": summaryText}
+        summary = generate_summary(transcript)
+    except Exception as e:
+        print(e)
+        summary = 'An error occurred generating summary.'
+        
+    print("Summary: ", summary)
+    return {"summary": summary}
     
 # for testing on terminal
 if __name__ == "__main__":
--- lca-ai-stack/source/lambda_functions/llm_anthropic_summary_lambda/index.py
+++ lca-ai-stack/source/lambda_functions/llm_anthropic_summary_lambda/index.py
@@ -19,9 +19,29 @@ ENDPOINT_URL = os.environ["ENDPOINT_URL"]
 FETCH_TRANSCRIPT_LAMBDA_ARN = os.environ['FETCH_TRANSCRIPT_LAMBDA_ARN']
 PROCESS_TRANSCRIPT = (os.getenv('PROCESS_TRANSCRIPT', 'False') == 'True')
 TOKEN_COUNT = int(os.getenv('TOKEN_COUNT', '0')) # default 0 - do not truncate.
-SUMMARY_PROMPT_TEMPLATE = os.environ["SUMMARY_PROMPT_TEMPLATE"]
+SUMMARY_PROMPT_TEMPLATE = os.getenv('SUMMARY_PROMPT_TEMPLATE','')
+SUMMARY_PROMPT_SSM_PARAMETER = os.environ["SUMMARY_PROMPT_SSM_PARAMETER"]
 
 lambda_client = boto3.client('lambda')
+ssmClient = boto3.client("ssm")
+
+def get_templates_from_ssm():
+    global SUMMARY_PROMPT_TEMPLATE
+    templates = []
+    try:
+        SUMMARY_PROMPT_TEMPLATE = ssmClient.get_parameter(Name=SUMMARY_PROMPT_SSM_PARAMETER)["Parameter"]["Value"]
+
+        prompt_templates = json.loads(SUMMARY_PROMPT_TEMPLATE)
+        for k, v in prompt_templates.items():
+            prompt = v.replace("<br>", "\n")
+            templates.append({ k:prompt })
+    except:
+        prompt = SUMMARY_PROMPT_TEMPLATE.replace("<br>", "\n")
+        templates.append({
+            "Summary": prompt
+        })
+        print("Prompt: ",prompt)
+    return templates
 
 def get_transcripts(callId):
     payload = {
@@ -38,6 +58,36 @@ def get_transcripts(callId):
     print("Lambda response:", response)
     return response
 
+
+def generate_anthropic_summary(transcript):
+    # first check to see if this is one prompt, or many prompts as a json
+    templates = get_templates_from_ssm()
+    result = {}
+    for item in templates:
+        key = list(item.keys())[0]
+        prompt = item[key]
+        prompt = prompt.replace("{transcript}", transcript)
+        data = {
+            "prompt": prompt,
+            "model": ANTHROPIC_MODEL_IDENTIFIER,
+            "max_tokens_to_sample": 512,
+            "stop_sequences": ["Human:", "Assistant:"]
+        }
+        headers = {
+            "x-api-key": ANTHROPIC_API_KEY,
+            "content-type": "application/json"
+        }
+        response = requests.post(ENDPOINT_URL, headers=headers, data=json.dumps(data))
+        print("API Response:", response)
+        summary = json.loads(response.text)["completion"].strip()
+        result[key] = summary
+    if len(result.keys()) == 1:
+        # there's only one summary in here, so let's return just that.
+        # this may contain json or a string.
+        return result[list(result.keys())[0]]
+    return json.dumps(result)
+
+
 def handler(event, context):
     print("Received event: ", json.dumps(event))
     callId = event['CallId']
@@ -46,31 +96,15 @@ def handler(event, context):
     print("Transcript data:", transcript_data)
     transcript_json = json.loads(transcript_data)
     transcript = transcript_json['transcript']
-    prompt = SUMMARY_PROMPT_TEMPLATE.replace("<br>", "\n").replace("{transcript}", transcript)
-    print("Prompt: ",prompt)
-    data = {
-        "prompt": prompt,
-        "model": ANTHROPIC_MODEL_IDENTIFIER,
-        "max_tokens_to_sample": 512,
-        "stop_sequences": ["Human:", "Assistant:"]
-    }
-    headers = {
-        "x-api-key": ANTHROPIC_API_KEY,
-        "content-type": "application/json"
-    }
     
     try:
-        response = requests.post(ENDPOINT_URL, headers=headers, data=json.dumps(data))
-        response.raise_for_status()
-        print("API Response:", response)
-        summaryText = json.loads(response.text)["completion"].strip()
+        summaryText = generate_anthropic_summary(transcript)
         print("Summary: ", summaryText)
         return {"summary": summaryText}
     except requests.exceptions.HTTPError as err:
         logger.error(err)
         raise
 
-    
 # for testing on terminal
 if __name__ == "__main__":
     event = {
