7ea07dfe1be5dc4ef9b9387c2c67aff8b08404b4
Babu Srinivasan <babusri@amazon.com>
2024-05-17 15:11:27 +0000

    fix import

--- lca-websocket-stack/source/app/src/utils.ts
+++ lca-websocket-stack/source/app/src/utils.ts
@@ -1,6 +1,5 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-import fs from 'fs';
 
 export const posixifyFilename = function (filename: string) {
     // Replace all invalid characters with underscores.
